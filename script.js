3b63968c00e922d7064d177a7ed881aef733905f
Themis <themisgcp@gmail.com>
2023-11-30 15:46:55 +0100

    update

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ document.addEventListener("DOMContentLoaded", function () {
     passwordError.style.display = "none";
   });
 });
+
+//Sign In
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    fetch("http://localhost:8000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  });
